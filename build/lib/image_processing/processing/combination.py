7c5b73f387313e28fa28b123a68623cbcc6e19c4
sidneifs <supersidnei@gmail.com>
2025-09-14 14:04:19 -0300

    Adiciona funções de processamento de imagens, incluindo comparação, transferência de histograma e redimensionamento, além de métodos para leitura, salvamento e visualização de imagens.

---
+++ build/lib/image_processing/processing/combination.py
@@ -0,0 +1,22 @@
+import numpy as np
+from skimage.color import rgb2gray
+from skimage.exposure import match_histograms
+from skimage.metrics import structural_similarity
+
+def find_difference(image1, image2):
+    assert image1.shape == image2.shape, "Specify 2 images with de same shape."
+    gray_image1 = rgb2gray(image1)
+    gray_image2 = rgb2gray(image2)
+    # Define data_range para imagens float
+    if np.issubdtype(gray_image1.dtype, np.floating):
+        data_range = gray_image1.max() - gray_image1.min()
+        score, difference_image = structural_similarity(gray_image1, gray_image2, full=True, data_range=data_range)
+    else:
+        score, difference_image = structural_similarity(gray_image1, gray_image2, full=True)
+    print("Similarity of the images:", score)
+    normalized_difference_image = (difference_image-np.min(difference_image))/(np.max(difference_image)-np.min(difference_image))
+    return normalized_difference_image
+
+def transfer_histogram(image1, image2):
+    matched_image = match_histograms(image1, image2, multichannel=True)
+    return matched_image
